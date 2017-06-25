aa9d390e4e78abde83be786d933ec4fdbe2e08be
naime <hossain.naime@yahoo.com>
2017-06-25 17:25:51 +0600

    project init controller model migration created

---
+++ public/js/custom.js
@@ -0,0 +1,24 @@
+  $(document).ready(function() { // makes sure the whole site is loaded
+         
+
+
+      
+        
+          //preloader
+          $('#status').fadeOut(); // will first fade out the loading animation
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+            $('body').delay(350).css({'overflow':'visible'});
+
+
+     
+
+             //nicescroll
+         $("html").niceScroll({
+               cursorcolor:"#002c53"
+        });
+             //                //datepicker
+             // $('.datepicker').datepicker({
+             //  weekStart:1
+             //  });
+        });   
+
